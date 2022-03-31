0ff3dcf04579b4ceab5d3d05131f60a38d027389
LifetimeNine <2390904403@qq.com>
2022-03-31 18:05:10 +0800

    修改上传组件样式

--- src/components/Upload/index.vue
+++ src/components/Upload/index.vue
@@ -9,19 +9,32 @@
     :accept="accept"
   >
     <slot :url="modelValue">
-      <el-button :size="buttonSize" :disabled="disabled">
-        <svg-icon v-if="status === 1" :icon-class="icon" />
-        <svg-icon v-else-if="status === 5" :icon-class="icon" style="color: #67C23A" />
-        <svg-icon v-else-if="status === 6" :icon-class="icon" style="color: #F56C6C" />
-        <svg-icon v-else :icon-class="icon" />
-        <span style="margin-left: 2px">{{ btn_txt }}</span>
+      <el-button v-if="type === 'button'" :size="buttonSize" :disabled="disabled">
+        <svg-icon v-if="status === 1" :icon-class="btnIcon" />
+        <svg-icon v-else-if="status === 6" :icon-class="btnIcon" style="color: #67C23A" />
+        <svg-icon v-else-if="status === 7" :icon-class="btnIcon" style="color: #F56C6C" />
+        <svg-icon v-else :icon-class="btnIcon" />
+        <span v-if="status === 1 || status === 6 || status === 7" style="margin-left: 2px">{{ action }}</span>
+        <span v-else style="margin-left: 2px">{{ action }}：{{ progress }}%</span>
       </el-button>
+      <el-image v-if="type === 'image'" :src="modelValue" fit="fill" :lazy="true" :style="{ width: imageWidth, height: imageHeight }" :class="{ 'hide-border': status !== 1 }">
+        <template #error>
+          <svg-icon v-show="status === 1" icon-class="plus" />
+        </template>
+      </el-image>
+      <el-progress v-if="type === 'image' && ![1, 5, 6].includes(status)" type="circle" :percentage="progress">
+        <template #default>
+          <div>{{ action }}</div>
+          <div>{{ progress }}%</div>
+        </template>
+      </el-progress>
     </slot>
   </el-upload>
 </template>
 
 <script>
 import Upload from '@/utils/upload'
+import { percentage } from '@/utils'
 export default {
   name: 'Upload',
   props: {
@@ -37,32 +50,69 @@ export default {
     accept: {
       type: String,
       default: '*'
+    },
+    type: {
+      type: String,
+      default: 'button',
+      validator(value) {
+        return ['button', 'image'].includes(value)
+      }
+    },
+    imageHeight: {
+      type: String,
+      default: '120px'
+    },
+    imageWidth: {
+      type: String,
+      default: '120px'
+    },
+    multiple: {
+      type: Boolean,
+      default: false
     }
   },
   emits: ['update:modelValue', 'on-status', 'on-progress', 'on-success', 'on-fail'],
   data() {
     return {
-      btn_txt: '选择文件',
+      list: [],
+      progress: 0,
+      action: '选择文件',
       status: 1,
-      icon: 'upload',
+      btnIcon: 'upload',
       disabled: false
     }
   },
   watch: {
     status: function(value) {
       this.$emit('on-status', value)
-      if (value === 1) {
-        this.icon = ''
-        this.disabled = false
-      } else if (value === 5) {
-        this.icon = 'check'
-        this.disabled = false
-      } else if (value === 6) {
-        this.icon = 'close'
-        this.disabled = false
-      } else {
-        this.icon = 'loading'
-        this.disabled = true
+      switch (value) {
+        case 1:
+          this.btnIcon = 'upload'
+          this.disabled = false
+          this.action = '选择文件'
+          break
+        case 2:
+        case 3:
+          this.btnIcon = 'loading'
+          this.disabled = true
+          this.action = 'MD5'
+          break
+        case 4:
+        case 5:
+          this.btnIcon = 'loading'
+          this.disabled = true
+          this.action = '上传'
+          break
+        case 6:
+          this.btnIcon = 'check'
+          this.action = '重新选择'
+          this.disabled = false
+          break
+        case 7:
+          this.btnIcon = 'warning'
+          this.action = '重新选择'
+          this.disabled = false
+          break
       }
     }
   },
@@ -72,33 +122,31 @@ export default {
       this.$emit('on-status', 1)
       const upload = new Upload()
       upload.onMd5Before(() => {
-        this.btn_txt = `MD5: 0%`
+        this.progress = 0
         this.status = 2
       }).onMd5Progress((loaded, total) => {
-        const progress = Math.round(loaded / total * 10000) / 100
-        this.btn_txt = `MD5: ${progress}%`
-        this.$emit('on-progress', 1, progress)
+        this.progress = percentage(loaded, total, false)
+        this.$emit('on-progress', 1, this.progress)
       }).onMd5After(() => {
-      }).onBefore(() => {
-        this.btn_txt = `上传: 0%`
+        this.progress = 100
         this.status = 3
-      }).onProgress((loaded, total) => {
-        const progress = Math.round(loaded / total * 10000) / 100
-        this.btn_txt = `上传: ${progress}%`
-        this.$emit('on-progress', 2, progress)
-      }).onAfter(() => {
-        this.btn_txt = '合并中'
+      }).onBefore(() => {
+        this.progress = 0
         this.status = 4
+      }).onProgress((loaded, total) => {
+        this.progress = percentage(loaded, total, false)
+        this.$emit('on-progress', 2, this.progress)
+      }).onAfter(() => {
+        this.progress = 100
+        this.status = 5
       })
       const saveFunc = this.part ? upload.partSave : upload.save
       saveFunc(raw).then(url => {
-        this.btn_txt = '重新选择'
-        this.status = 5
+        this.status = 6
         this.$emit('update:modelValue', url)
         this.$emit('on-success', url)
       }).catch(() => {
-        this.btn_txt = '重新选择'
-        this.status = 6
+        this.status = 7
         this.$emit('on-fail')
       })
     },
@@ -111,3 +159,24 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  .el-image {
+    border: 1px dashed #d9d9d9;
+    &.hide-border {
+      border-width: 0;
+    }
+    &:hover {
+      border-color: var(--el-color-primary);;
+    }
+    .svg-icon {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+  .el-progress {
+    position: absolute;
+  }
+</style>
--- src/views/demo/Upload.vue
+++ src/views/demo/Upload.vue
@@ -5,6 +5,10 @@
       v-model 绑定url值
       part    是否是切片上传
       accept  接受上传的文件类型
+      type    展示类型 button-按钮，image-图片
+      image-width   图片宽
+      image-height  图片高
+      multiple  是否支持多选文件
       on-status 状态改变事件 (status) 1-未开始 2-正在计算MD5 3-正在上传 4-正在合并 5-上传成功 6-上传失败
       on-progress 上传进度事件 (type, progress) type:类型 1-MD5计算进度 2-上传进度；progress：进度百分比
       on-success  上传成功事件 (success)
@@ -14,6 +18,8 @@
       ref="upload"
       v-model="url"
       :part="true"
+      type="image"
+      :multiple="true"
       @on-status="onStatus"
       @on-progress="onProgress"
       @on-success="onSuccess"
